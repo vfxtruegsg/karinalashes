a16ec643247a370b906679ac990bd2846e06e077
Andrij P. <andrejpopkov565@gmail.com>
2024-11-14 23:01:06 +0300

    gallery work

--- src/js/gallery-work.js
+++ src/js/gallery-work.js
@@ -1,24 +1,34 @@
-// import Swiper from 'swiper';
-// import 'swiper/css';
+import Swiper from 'swiper';
+import 'swiper/css';
 
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-// });
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  slidesPerView: 2, // По умолчанию отображается 1 слайд
+  spaceBetween: 10,
+  breakpoints: {
+    768: {
+      slidesPerView: 2, // 2 слайда на экранах от 768px
+      spaceBetween: 15,
+    },
+    1024: {
+      slidesPerView: 3, // 3 слайда на экранах от 1024px
+      spaceBetween: 20,
+    },
+    1440: {
+      slidesPerView: 4, // 4 слайда на экранах от 1440px
+      spaceBetween: 30,
+    },
+  },
+});
