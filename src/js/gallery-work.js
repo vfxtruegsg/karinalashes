11752794f1b9867c3342a624e06b76e53e5f5333
Andrij P. <andrejpopkov565@gmail.com>
2024-11-15 13:50:05 +0300

    settings gallery, add section services

--- src/js/gallery-work.js
+++ src/js/gallery-work.js
@@ -1,7 +1,7 @@
 import Swiper from 'swiper';
 import 'swiper/css';
 
-const swiper = new Swiper('.swiper', {
+var swiper = new Swiper('.swiper', {
   loop: true,
   pagination: {
     el: '.swiper-pagination',
@@ -15,20 +15,49 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
     draggable: true,
   },
-  slidesPerView: 2, // По умолчанию отображается 1 слайд
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 2,
   spaceBetween: 10,
   breakpoints: {
     768: {
-      slidesPerView: 2, // 2 слайда на экранах от 768px
+      slidesPerView: 2,
       spaceBetween: 15,
     },
     1024: {
-      slidesPerView: 3, // 3 слайда на экранах от 1024px
+      slidesPerView: 3,
       spaceBetween: 20,
+      mousewheel: {
+        forceToAxis: true,
+      },
     },
     1440: {
-      slidesPerView: 4, // 4 слайда на экранах от 1440px
+      slidesPerView: 4,
       spaceBetween: 30,
+      mousewheel: {
+        forceToAxis: true,
+      },
     },
   },
 });
+
+const gallery = document.querySelector('.swiper');
+
+gallery.addEventListener(
+  'wheel',
+  event => {
+    // Предотвращаем стандартное поведение прокрутки страницы
+    event.preventDefault();
+
+    // Если колесо прокручивается вниз — переходим к следующему слайду
+    if (event.deltaY > 0) {
+      swiper.slideNext();
+    } else {
+      // Если колесо прокручивается вверх — переходим к предыдущему слайду
+      swiper.slidePrev();
+    }
+  },
+  { passive: false }
+);
